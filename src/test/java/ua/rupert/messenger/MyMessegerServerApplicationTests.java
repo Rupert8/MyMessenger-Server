0f0c0034004d0d3e897c5dd0eb49e01905b8b6b1
Anton <antonsavcenko128@gmail.com>
2025-10-22 16:47:53 +0300

    init commit

---
+++ src/test/java/ua/rupert/messenger/MyMessegerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.rupert.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMessegerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
